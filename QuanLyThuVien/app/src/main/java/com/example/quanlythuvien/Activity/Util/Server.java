42360376e2bff145f84fde02aef53a853ad8fc20
huykt1111 <huylmht10@gmail.com>
2021-12-10 20:53:47 +0700

    a

---
+++ QuanLyThuVien/app/src/main/java/com/example/quanlythuvien/Activity/Util/Server.java
@@ -0,0 +1,7 @@
+package com.example.quanlythuvien.Activity.Util;
+
+public class Server {
+    public static String localhost="192.168.1.109";
+    public static String Duongdankesach = "http://" + localhost + "/thuvienute/getkesach.php";
+
+}
