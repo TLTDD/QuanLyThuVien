7fca9de86c7f84c795f01395320ec3e68e1b0728
huykt1111 <huylmht10@gmail.com>
2021-12-10 22:59:58 +0700

    a

---
+++ QuanLyThuVien/app/src/main/java/com/example/quanlythuvien/Activity/Adapter/SachAdapter.java
@@ -0,0 +1,73 @@
+package com.example.quanlythuvien.Activity.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.quanlythuvien.Activity.Model.Sach;
+import com.example.quanlythuvien.R;
+import com.squareup.picasso.Picasso;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+
+public class SachAdapter extends RecyclerView.Adapter<SachAdapter.ItemHolder> {
+    Context context;
+    ArrayList<Sach> arraySach;
+
+    public SachAdapter(Context context, ArrayList<Sach> arraySach) {
+        this.context = context;
+        this.arraySach = arraySach;
+    }
+
+    @NonNull
+    @Override
+    public ItemHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_sach,null);
+        ItemHolder itemHolder = new ItemHolder(v);
+        return itemHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ItemHolder holder, int position) {
+        Sach sach = arraySach.get(position);
+        holder.tvTenSach.setText(sach.getTenSach());
+        DecimalFormat decimalFormat = new DecimalFormat("###,###,###");
+        holder.tvGiaSach.setText("Giá: "+decimalFormat.format(sach.getGiaSach())+" Đ");
+        Picasso.get().load(sach.getHinhAnhSach())
+                .placeholder(R.drawable.noimage)
+                .error(R.drawable.error)
+                .into(holder.imgSach);
+    }
+
+    @Override
+    public int getItemCount() {
+        return arraySach.size();
+    }
+
+
+    public class ItemHolder extends RecyclerView.ViewHolder{
+        public ImageView imgSach;
+        public TextView tvTenSach,tvTacGia,tvGiaSach;
+
+        public ItemHolder(View itemView) {
+            super(itemView);
+            imgSach = itemView.findViewById(R.id.imgSach);
+            tvTenSach = itemView.findViewById(R.id.tvTenSach);
+            tvTacGia = itemView.findViewById(R.id.tvTenTacGia);
+            tvGiaSach = itemView.findViewById(R.id.tvGiaSach);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
+        }
+    }
+}
--- QuanLyThuVien/app/src/main/java/com/example/quanlythuvien/Activity/Fragment/MuonFragment.java
+++ QuanLyThuVien/app/src/main/java/com/example/quanlythuvien/Activity/Fragment/MuonFragment.java
@@ -8,13 +8,24 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.quanlythuvien.Activity.Model.Sach;
 import com.example.quanlythuvien.R;
 
+import java.util.ArrayList;
+
 public class MuonFragment extends Fragment {
+    View view;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_muon,container,false);
+        view = inflater.inflate(R.layout.fragment_muon,container,false);
+        AnhXa();
+        return view;
+    }
+
+    private void AnhXa() {
+
     }
 }
