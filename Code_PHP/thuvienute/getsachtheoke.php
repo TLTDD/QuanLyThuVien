bd8ba29c1f15552a3e8ecfafe31acddcb3f5e27a
huykt1111 <huylmht10@gmail.com>
2021-12-11 07:25:42 +0700

    a

--- Code_PHP/thuvienute/getsachtheoke.php
+++ Code_PHP/thuvienute/getsachtheoke.php
@@ -1,22 +1,23 @@
 <?php
     include "connect.php";
+    $idKeSach = $_GET['idKeSach'];
     $mangsach = array();
-    $query = 'SELECT * FROM sach';
+    $query = "SELECT * FROM sach WHERE idKeSach = '$idKeSach'";
     $data = mysqli_query($conn, $query);
     while ($row = mysqli_fetch_assoc($data)){
         array_push($mangsach,new Sach(
-                $row['id'],
-                $row['idKeSach'],
-                $row['tenSach'],
-                $row['tenTacGia'],
-                $row['nhaXuatBan'],
-                $row['ngayXuatBan'],
-                $row['ngonNgu'],
-                $row['giaSach'],
-                $row['soSeries'],
-                $row['soLuong'],
-                $row['loiTua'],
-                $row['hinhAnhSach']));
+            $row['id'], 
+            $row['idKeSach'],
+            $row['tenSach'],
+            $row['tenTacGia'],
+            $row['nhaXuatBan'],
+            $row['ngayXuatBan'],
+            $row['ngonNgu'],
+            $row['giaSach'],
+            $row['soSeries'],
+            $row['soLuong'],
+            $row['loiTua'],
+            $row['hinhAnhSach']));
     }
     echo json_encode($mangsach);
     
