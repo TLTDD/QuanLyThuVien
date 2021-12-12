c2683b835dfeca09eec7f466f6eb63e95cb23812
huykt1111 <huylmht10@gmail.com>
2021-12-12 14:49:23 +0700

    a

--- QuanLyThuVien/app/src/main/java/com/example/quanlythuvien/Activity/Util/Server.java
+++ QuanLyThuVien/app/src/main/java/com/example/quanlythuvien/Activity/Util/Server.java
@@ -5,4 +5,5 @@ public class Server {
     public static String Duongdankesach = "http://" + localhost + "/thuvienute/getkesach.php";
     public static String Duongdansach = "http://" + localhost + "/thuvienute/getsach.php";
     public static String Duongdansachtheoke = "http://" + localhost + "/thuvienute/getsachtheoke.php?idKeSach=";
+    public static String Duongdanbandoc = "http://" + localhost + "/thuvienute/getbandoc.php";
 }
