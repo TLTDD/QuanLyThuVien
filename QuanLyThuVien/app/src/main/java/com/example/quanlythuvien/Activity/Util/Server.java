8dbb6c09ab8bac05d4b6396d23279640ef3d793c
sybuivan <sybuivan1429@gmail.com>
2021-12-21 20:27:51 +0700

    ad

--- QuanLyThuVien/app/src/main/java/com/example/quanlythuvien/Activity/Util/Server.java
+++ QuanLyThuVien/app/src/main/java/com/example/quanlythuvien/Activity/Util/Server.java
@@ -1,7 +1,7 @@
 package com.example.quanlythuvien.Activity.Util;
 
 public class Server {
-    public static String localhost="192.168.1.109";
+    public static String localhost="192.168.1.4";
     public static String Duongdankesach = "http://" + localhost + "/thuvienute/getkesach.php";
     public static String Duongdansach = "http://" + localhost + "/thuvienute/getsach.php";
     public static String Duongdansachtheoke = "http://" + localhost + "/thuvienute/getsachtheoke.php?idKeSach=";
